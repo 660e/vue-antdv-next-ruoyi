a6a6c56da197ae8e18460d3aaf650d195d81fbef
Min <solunar@vip.qq.com>
2026-08-26 21:48:03 +0800

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,29 @@ import { fileURLToPath, URL } from 'node:url';
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
 import { defineConfig } from 'vite';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), vue(), vueJsx(), vueDevTools()],
+  plugins: [
+    // https://github.com/unplugin/unplugin-auto-import
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+    }),
+
+    // https://github.com/unplugin/unplugin-vue-components
+    Components({}),
+
+    // https://tailwindcss.com/docs/installation/using-vite
+    tailwindcss(),
+
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
