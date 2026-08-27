961f0706f38a477e926120705953bd14f3386199
Min <solunar@vip.qq.com>
2026-08-27 08:45:53 +0800

    Update

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,11 @@ const router = createRouter({
       name: 'home',
       component: () => import('@/views/home.vue'),
     },
+    {
+      path: '/playground',
+      name: 'playground',
+      component: () => import('@/views/playground/index.vue'),
+    },
   ],
 });
 
---
+++ src/views/playground/index.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { is } from "@/utils";
+</script>
+
+<template>
+  <div>
+    <div>{{ is.array([1, 2, 3]) }} {{ is.array(undefined) }}</div>
+  </div>
+</template>
